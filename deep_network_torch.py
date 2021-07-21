a4e0486c1e7fdf55d3dd7c46ac8352c2c34d58d6
Trapti04 <trapti.kalra@gmail.com>
2021-07-21 20:36:36 +0530

    - changed file name

---
+++ deep_network_torch.py
@@ -0,0 +1,43 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor, Lambda, Compose
+import matplotlib.pyplot as plt
+
+def download_data():
+    # Download training data from open datasets.
+    training_data = datasets.FashionMNIST(
+        root="data",
+        train=True,
+        download=True,
+        transform=ToTensor(),
+    )
+
+    # Download test data from open datasets.
+    test_data = datasets.FashionMNIST(
+        root="data",
+        train=False,
+        download=True,
+        transform=ToTensor(),
+    )
+    return training_data, test_data
+
+def createDataloaders(training_data, test_data, BATCH_SIZE = 64 ):
+    batch_size = BATCH_SIZE
+
+    # Create data loaders.
+    train_dataloader = DataLoader(training_data, batch_size=batch_size)
+    test_dataloader = DataLoader(test_data, batch_size=batch_size)
+
+    for X, y in test_dataloader:
+        print("Shape of X [N, C, H, W]: ", X.shape)
+        print("Shape of y: ", y.shape, y.dtype)
+        break
+    return train_dataloader, test_dataloader
+
+def main():
+    if __name__ == "__main__":
+        training_data, test_data = download_data()
+        train_dataloader, test_dataloader = createDataloaders(training_data, test_data, BATCH_SIZE = 64 )
+
